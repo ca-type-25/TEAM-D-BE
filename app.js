ce2fa16352be87356eb0b5ee12d930e12626e325
fellow-Git-user <martynas.nadeika@gmail.com>
2025-04-15 11:53:16 +0300

    data base added, and structure scaffolded

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('veikia....')
+
+const express = require('express')
+require('dotenv').config()
+const process = require('process')
+require('./db')
+
+const cors = require('cors')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
