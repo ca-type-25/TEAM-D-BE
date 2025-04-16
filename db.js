da9fb201e0f7ec13c36b3a77d484a0fd7da2c8e5
Sigitaali <alisauskienesigita@gmail.com>
2025-04-16 08:36:50 +0300

    bug

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config(); 
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.DB_URI)
+
     .then(() => {
         console.log('Suup, Successfully connected to MongoDB:')
     })
