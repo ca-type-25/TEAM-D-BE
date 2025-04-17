65383d21ff166c63b4dbef7d57b1bf36f59eb84e
RobertasJakaitis99 <1113332220z@gmail.com>
2025-04-17 07:15:35 +0300

    Add destination CRUD: model, controller, routes

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require('express')
-require('dotenv').config()
-const process = require('process')
-require('./db')
+const express = require("express");
+require("dotenv").config();
+const process = require("process");
+require("./db");
 
-const cors = require('cors')
+const cors = require("cors");
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+// ROUTES
+const tripAPIRoutes = require("./ApiRoutes/trips");
+const destinationAPIRoutes = require("./ApiRoutes/destinations");
 
-const tripAPIRoutes = require('./ApiRoutes/trips')
-app.use('/api/trips', tripAPIRoutes)
+app.use("/api/trips", tripAPIRoutes);
+app.use("/api/destinations", destinationAPIRoutes);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
