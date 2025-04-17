bd0b112c80c27831167abcbe17afff1453beb7bf
DomasPauliukas <Domas0319@gmail.com>
2025-04-17 09:53:01 +0300

    update

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
 const express = require('express')
-const process = require("process");
+const process = require("process")
 const cors = require('cors')
-const app = express();
+const app = express()
 require('dotenv').config()
 require('./db')
 
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 
 // ROUTES
-const tripAPIRoutes = require("./ApiRoutes/trips");
-const destinationAPIRoutes = require("./ApiRoutes/destinations");
-
-
-const userRoutes = require('./ApiRoutes/users')
-app.use('/api/users', userRoutes)
-
-
+const tripAPIRoutes = require('./ApiRoutes/trips')
+const destinationAPIRoutes = require('./ApiRoutes/destinations')
 const activitiesRouter = require('./ApiRoutes/activities')
 const countriesRouter = require('./ApiRoutes/countries')
+const userRoutes = require('./ApiRoutes/users')
 
-
-
+app.use('/api/destinations', destinationAPIRoutes)
+app.use('/api/trips', tripAPIRoutes)
+app.use('/api/users', userRoutes)
 app.use('/api/activities', activitiesRouter)
 app.use('/api/countries', countriesRouter)
 
 
-const tripAPIRoutes = require('./ApiRoutes/trips')
-app.use('/api/trips', tripAPIRoutes)
-
-
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-
-
-app.use("/api/destinations", destinationAPIRoutes);
-
