85be35b59bcdc9d0bf43d7b1fd476af403779719
Sigitaali <alisauskienesigita@gmail.com>
2025-04-23 09:03:15 +0300

    .

--- ApiRoutes/users.js
+++ ApiRoutes/users.js
@@ -10,7 +10,7 @@ const {
 
 const authMiddleware = require('../middlewares/authMiddleware')
 
-router.post('/register', register)
+router.post('/register', register);
 
 router.post('/login', login)
 
